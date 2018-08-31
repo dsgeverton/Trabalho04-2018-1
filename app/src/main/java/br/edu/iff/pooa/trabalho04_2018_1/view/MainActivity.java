1bbcf14a1ad39e5594016953a5ff9ff8cfb804b1
Everton Gonçalves <evertongoncalves@campos.rj.gov.br>
2018-08-31 16:29:00 -0300

    Move MainActivity to view package

---
+++ app/src/main/java/br/edu/iff/pooa/trabalho04_2018_1/view/MainActivity.java
@@ -0,0 +1,103 @@
+package br.edu.iff.pooa.trabalho04_2018_1.view;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
+
+import br.edu.iff.pooa.trabalho04_2018_1.R;
+import br.edu.iff.pooa.trabalho04_2018_1.adapter.MarcaAdapter;
+import br.edu.iff.pooa.trabalho04_2018_1.configRetrofit.FipeRetroFit;
+import br.edu.iff.pooa.trabalho04_2018_1.model.Marca;
+import br.edu.iff.pooa.trabalho04_2018_1.service.ClickRecyclerViewListener;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements ClickRecyclerViewListener {
+
+    RecyclerView mRecyclerView;
+    Call<List<Marca>> call;
+    List<Marca> marcas;
+    private ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setCancelable(false);
+        dialog.setMessage("Atualizando...");
+
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.veiculos_array,
+                                                                            android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinnerVeiculo = findViewById(R.id.spinnerVeiculo);
+        mRecyclerView = findViewById(R.id.rvMarcas);
+
+        spinnerVeiculo.setAdapter(adapter);
+        spinnerVeiculo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(final AdapterView<?> adapterView, View view, final int i, long l) {
+                dialog.show();
+                String marca = null;
+                switch (i){
+                    case 0: marca = "carros";
+                            break;
+                    case 1: marca = "motos";
+                            break;
+                    case 2: marca = "caminhoes";
+                            break;
+                }
+                if (marca != null){
+                    call = new FipeRetroFit().getFipeService().buscarMarcas(marca);
+                    call.enqueue(new Callback<List<Marca>>() {
+                        @Override
+                        public void onResponse(Call<List<Marca>> call, Response<List<Marca>> response) {
+                            marcas = response.body();
+                            Log.i("---------------", "MARCAS QTD:" + marcas.size());
+
+                            obterAdapter(marcas, i);
+                            dialog.dismiss();
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<Marca>> call, Throwable t) {
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+    protected void obterAdapter(List<Marca> marcas, int vehicle) {
+        super.onResume();
+        mRecyclerView.setAdapter(new MarcaAdapter(vehicle, marcas,getBaseContext(), this));
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(getBaseContext(),
+                LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layout);
+    }
+
+    @Override
+    public void onClick(Object object) {
+
+    }
+}
+
+
