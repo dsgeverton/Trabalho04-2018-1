4968454ab0337595e31a8600267b2d35b0cb6d58
Everton <dsgeverton@gmail.com>
2018-08-31 09:39:58 -0300

    Add model class Marca

---
+++ app/src/main/java/br/edu/iff/pooa/trabalho04_2018_1/model/Marca.java
@@ -0,0 +1,23 @@
+package br.edu.iff.pooa.trabalho04_2018_1.model;
+
+public class Marca {
+
+    private long codigo;
+    private String nome;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
