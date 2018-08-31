b57db4c71184823b58f9a33424ecaf660fe57de4
Everton <dsgeverton@gmail.com>
2018-08-31 10:43:51 -0300

    Create spinner to select vehicle type

--- app/src/main/java/br/edu/iff/pooa/trabalho04_2018_1/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa/trabalho04_2018_1/MainActivity.java
@@ -2,6 +2,11 @@ package br.edu.iff.pooa.trabalho04_2018_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.veiculos_array,
+                                                                            android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinnerVeiculo = findViewById(R.id.spinnerVeiculo);
+
+        spinnerVeiculo.setAdapter(adapter);
+        spinnerVeiculo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getBaseContext(), String.valueOf(adapterView.getItemIdAtPosition(i)), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
